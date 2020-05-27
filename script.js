886bc647758238efb9437539ec27839723fbdde5
Martin Gras <martin.gras95@gmail.com>
2020-05-27 13:16:54 +0100

    delete and clear functions added

--- script.js
+++ script.js
@@ -5,11 +5,18 @@ $(document).ready(function () {
         e.preventDefault();
         // get name of desired new skill
         var newSkill = document.getElementById("txt-skill").value;
-        // add the new skill to local memory and set to false as a default
-        localStorage.setItem(newSkill,false);
-        // prepend a new row, column with the new skill and button inside 
-        // ugliest line ever
-        $(this).before("<div class="+`"row"`+">"+ "<div class="+`"col"`+">"+"<h3>"+newSkill+"</h3>" + "<button class="+`"btn btn-primary btn-learned"`+">Learned</button>"+"</div>"+"</div><br />");
+
+        // if text box is empty, dont add
+        if(newSkill !=""){
+            // add the new skill to local memory and set to false as a default
+            localStorage.setItem(newSkill.toLowerCase(),false);
+            // prepend a new row, column with the new skill and button inside 
+            // ugliest line ever
+            $(this).before("<div class="+`"row"`+">"+ "<div class="+`"col"`+">"+"<h3>"+newSkill+"</h3>" + "<button class="+`"btn btn-primary btn-learned"`+">Complete</button>"+"<button class="+`"btn btn-danger btn-delete"`+">Delete</button>"+"</div>"+"</div>");
+            $("#txt-skill").val('');
+        }else{
+            alert("Please input correct skill");
+        }
     });
 
     // When button is clicked its corresponding skill is marked as learned
@@ -20,6 +27,26 @@ $(document).ready(function () {
         $(this).siblings("h3").css("color","red");
         localStorage.setItem(value,true);
 
+    });
+
+    // delete skill
+    $(".skills-container").on('click','.btn-delete',function (e) { 
+        e.preventDefault();
+        var value = $(this).siblings("h3").text();
+        // remove the elements
+        $(this).parentsUntil(".skills-container").remove();
+        // remove from localstorage
+        localStorage.removeItem(value);
+    });
+
+    // Clear entire skill tree
+    $("#btn-clear").click(function (e) { 
+        e.preventDefault();
+        // clear localstorage
+        localStorage.clear();
+        // clear all children elements of skills container except ones with perm(permanent) class
+        $(".skills-container").children().not(".perm").remove();
+        
     });
 
 
