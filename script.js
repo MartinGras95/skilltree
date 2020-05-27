7a5429c649ad75ecd32e387c592da4628aec1c30
Martin Gras <martin.gras95@gmail.com>
2020-05-27 11:52:07 +0100

    adding skills functionality added - new buttons dont carry over event listener

--- script.js
+++ script.js
@@ -3,6 +3,15 @@
 // Create const for each learned button
 const learnedBtn = document.querySelector('.btn-learned');
 
+// add new skill
+$("#btn-add").click(function (e) { 
+    e.preventDefault();
+    var newSkill = document.getElementById("txt-skill").value;
+    console.log(newSkill);
+    // ugliest line ever
+    $(this).before("<div class="+`"row"`+">"+ "<div class="+`"col"`+">"+"<h3>"+newSkill+"</h3>" + "<button class="+`"btn btn-primary btn-learned"`+">Learned</button>"+"</div>"+"</div>");
+});
+
 // When button is clicked its corresponding skill is marked as learned
 // also local storage variable is set to true
 $(".btn-learned").click(function (e) { 
@@ -14,11 +23,7 @@ $(".btn-learned").click(function (e) {
 });
 
 
-// add new skill
-$("#btn-add").click(function (e) { 
-    e.preventDefault();
-    
-});
+
 
 
 
